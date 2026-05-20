e37cd062eef3dff9d983c986ee027bb86aa67274
k1rya <kirillkrasnov78@gmail.com>
2026-05-20 16:13:09 +0700

    #9 сделал EFrepository, ApplicationDBContext и создал папку с конфигами

---
+++ ApartmentsRentService/Infrastructure/ApartmentRentService.Infrastructure.EntityFramework/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using ApartmentsRentService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApartmentsRentService.Infrastructure.EntityFramework;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : DbContext(options)
+{
+    public DbSet<Apartment> Apartments { get; set; }
+
+    public DbSet<Availability> Availabilities { get; set; }
+
+    public DbSet<Booking> Bookings { get; set; }
+
+    public DbSet<Landlord> Landlords { get; set; }
+
+    public DbSet<Tenant> Tenants { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.EnableSensitiveDataLogging();
+
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
---
+++ ApartmentsRentService/Infrastructure/ApartmentRentService.Infrastructure.EntityFramework/Configurations/TenantConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApartmentsRentService.Infrastructure.EntityFramework.Configurations
+{
+    internal class TenantConfiguration
+    {
+    }
+}
