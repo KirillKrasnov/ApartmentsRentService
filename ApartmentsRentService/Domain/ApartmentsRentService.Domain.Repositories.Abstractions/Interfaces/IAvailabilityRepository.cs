6998fefe0dfe62c3fe3a626bff8d95449af02e01
k1rya <kirillkrasnov78@gmail.com>
2026-05-20 16:55:11 +0700

    Добавил один репозиторий

---
+++ ApartmentsRentService/Domain/ApartmentsRentService.Domain.Repositories.Abstractions/Interfaces/IAvailabilityRepository.cs
@@ -0,0 +1,8 @@
+﻿using ApartmentsRentService.Domain.Entities;
+using ApartmentsRentService.Domain.Repositories.Abstractions.Base;
+
+namespace ApartmentsRentService.Domain.Repositories.Abstractions.Interfaces;
+
+public interface IAvailabilityRepository : IRepository<Availability, Guid>
+{
+}
